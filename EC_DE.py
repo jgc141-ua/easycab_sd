7731b141c49ee82df57ba296e06c07334bfeeec9
Adrián Rodríguez-Barbero Castillo <arc174@alu.ua.es>
2024-10-13 13:29:04 +0200

    EC_DE implementado para una primera impresión, a falta de revisión

--- EC_DE.py
+++ EC_DE.py
@@ -3,34 +3,134 @@
 
 # Librerías
 import socket # Para establecer la conexión de red entre las aplicaciones del sistema
-import time # Para simular un comportamiento en tiempo real (pausas controladas en la ejecución)
 import sys # Para acceder a los argumentos de la línea de comandos
+from kafka import kafkaProducer, KafkaConsumer   # type: ignore
+import json
+import time
 
-# Función encargada de manejar la conexión con la central y esperar órdenes
+HEADER = 64
+FORMAT = 'utf-8'
+FIN = 'FIN'
+
+# Inicialización de la posición del taxi (indefinida)
+posicion_actual = None
+
+# Función encargada de manejar la conexión con la central y esperar órdenes, usando sockets
 def conexion_central(ip_central, port_central, id_taxi):
+    try:
+        # Creación de un socket IP/TCP
+            # AF_INET -> se usará IPv4
+            # SOCK_STREAM -> se usará TCP
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as socket_creado:
+            # Conexión del socket creado con el servidor (central de control) mediante la IP y PUERTO dados
+            socket_creado.connect((ip_central, int(port_central)))
 
-    # Creación de un socket IP/TCP
-        # AF_INET -> se usará IPv4
-        # SOCK_STREAM -> se usará TCP
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as socket_creado:
-        # Conexión del socket creado con el servidor (central de control) mediante la IP y PUERTO dados
-        socket_creado.connect((ip_central, port_central))
+            # Autenticación con la central enviando el ID del taxi
+            mensaje_de_autenticacion = f'AUTENTICAR TAXI = #{id_taxi}'
 
-        # Autenticación con la central enviando el ID del taxi
-        mensaje_de_autenticacion = f'AUTENTICAR TAXI = #{id_taxi}'
+            # Envío del mensaje de autenticación
+            socket_creado.sendall(mensaje_de_autenticacion.encode(FORMAT))
 
-        # Envío del mensaje de autenticación
-        socket_creado.sendall(mensaje_de_autenticacion.encode())
+            # Espera de la respuesta de la central
+            respuesta = socket_creado.recv(HEADER).decode(FORMAT)
 
-        # Espera de la respuesta de la central
-        respuesta = socket_creado.recv(1024).decode()
+            # Mostramos la respuesta de la central
+            print(f"Respuesta de la central: {respuesta}")
 
-        # Mostramos la respuesta de la central
-        print(f"Respuesta de la central: {respuesta}")
+            # Comprobamos si la autenticación fue válida o errónea
+            if respuesta == "OK":
+                print(f"Taxi {id_taxi} autenticado correctamente")
+                return socket_creado # Devolvemos el socket para usarlo después
+            else:
+                print(f"Fallo de autenticación del taxi {id_taxi}")
+                return None
+        
+    except ConnectionError:
+        print(f"Error de conexión con la central en {ip_central}:{port_central}")
+    except Exception as e:
+        print(f"Error generado: {str(e)}")
+        return None
+    
+# Función encargada de enviar eventos del taxi a través de kafka
+def enviar_eventos_kafka(productor, id_taxi, mensaje_kafka):
+    global posicion_actual
 
-        # Comprobamos si la autenticación fue válida o errónea
-        if respuesta == "OK":
-            print(f"Taxi {id_taxi} autenticado correctamente")
-        else:
-            print(f"Fallo de autenticación del taxi {id_taxi}")
-            return # Terminamos la conexión
+    if posicion_actual is not None:
+        evento = {
+            'tipo' : 'movimiento',
+            'id_taxi' : id_taxi,
+            'posicion' : f'x:{posicion_actual["x"]}, y:{posicion_actual["y"]}'
+        }
+
+        # Mensaje en kafka
+        productor.send(mensaje_kafka, value=evento)
+        productor.flush()
+
+        print(f"[EVENTO] Taxi {id_taxi} publicó su posición en kafka: {evento['posicion']}")
+    else:
+        print(f"[ERROR] Taxi no tiene una posición definida para enviar")
+
+# Función encargada de escuchar instrucciones desde kafka
+def recibir_mensajes_kafka(consumidor, id_taxi):
+    print(f"[ESPERANDO] Taxi {id_taxi} experando mensajes de la central...")
+
+    for mensaje in consumidor:
+        datos = mensaje.value
+        print(f"Mensaje recibido de la central: {datos}")
+
+        if datos['tipo'] == 'instruccion':
+            print(f"Instrucción recibida: {datos['instruccion']}")
+            if datos['instruccion'] == 'parar':
+                print(f"Taxi {id_taxi} se detiene")
+            elif datos['instruccion'] == 'reanudar':
+                print(f"Taxi {id_taxi} reanuda el proyecto")
+            elif datos['instruccion'] == 'mover':
+                posicion_nueva = datos['posicion_nueva']
+                posicion_actual = {'x': posicion_nueva['x'], 'y' : posicion_nueva['y']}
+                print(f"Taxi {id_taxi} movido a la nueva posición: {posicion_actual}")
+            elif datos['instruccion'] == FIN:
+                print(f"La central ha finalizado la conexión con el Taxi {id_taxi}")
+                break
+
+# Función principal de conexión del taxi
+def conexion_taxi(ip_central, port_central, ip_broker, port_broker, ip_sensores, port_sensores, id_taxi):
+    # Conexión con la central
+    socket_creado = conexion_central(ip_central, port_central, id_taxi)
+
+    if socket_creado:
+        # Inicializamos el productor y el consumidor de kafka
+        broker = f'{ip_broker}:{port_broker}'
+        productor = kafkaProducer(bootstrap_servers = [broker], value_serializer = lambda v: json.dumps(v).encode(FORMAT))
+
+        topic_actualizacion_central = "actualizacion_central"
+        topic_actualizacion_taxi = f"actualización_taxi_{id_taxi}"
+
+        consumidor = KafkaConsumer(topic_actualizacion_taxi, 
+                                   bootstrap_server = [broker],
+                                   auto_offset_reset = 'earliest',
+                                   value_deserializer = lambda x: json.loads(x.decode(FORMAT)))
+        
+        # Mientras la conexión se encuentre activa, enviamos eventos y recibimos instrucciones
+        while True:
+            enviar_eventos_kafka(productor, id_taxi, topic_actualizacion_central)
+            recibir_mensajes_kafka(consumidor, id_taxi)
+            time.sleep(1)
+
+# Main
+if __name__ == "__main__":
+    # Se esperan 8 argumentos
+    if len(sys.argv) == 8:
+        ip_central = sys.argv[1]
+        port_central = sys.argv[2]
+        ip_broker = sys.argv[3]
+        port_broker = sys.argv[4]
+        ip_sensores = sys.argv[5]
+        port_sensores = sys.argv[6]
+        id_taxi = sys.argv[7]
+
+        # Conexión del taxi con la central y kafka
+        conexion_taxi(ip_central, port_central, ip_broker, port_broker, ip_sensores, port_sensores, id_taxi)
+
+    else:
+        print(f"ERROR: número de argumentos incorrecto")
+
